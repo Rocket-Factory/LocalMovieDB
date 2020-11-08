9272e9dce29c3a02596ddaed1ab36d4611ceda4b
taosky <taosky@nas.loca>
2020-11-08 21:14:38 +0800

    fix special movie title

--- douban.py
+++ douban.py
@@ -7,21 +7,27 @@ from retrying import retry
 import logging
 
 
-@retry(stop_max_attempt_number=5, wait_fixed=1000)
+@retry(stop_max_attempt_number=3, wait_fixed=1000)
 def get_db_id(name, year):
+    true_name = name
+    # 处理特殊符号
+    for index,s in enumerate(name):
+        if s in [',', ':','(','[']:
+            name = name[:index]
+            break
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Ailurus/68.0'}
     url = 'https://alagorn.8610000.xyz/api/v1/movies/suggest_query?q={}'.format(name)
     r = requests.get(url, headers=headers)
-    r.encoding = 'utf-8'
-    info_json = json.loads(r.text)
-    for movie_info in info_json:
-        if movie_info['title'] == name and movie_info['year'] == year:
+    if 'code' in r.json():
+        return None
+    for movie_info in r.json():
+        if movie_info['title'] == true_name and movie_info['year'] == year:
             return movie_info['id']
     return None
 
 
-@retry(stop_max_attempt_number=5, wait_fixed=1000)
+@retry(stop_max_attempt_number=3, wait_fixed=1000)
 def get_db_info(subject_id):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Ailurus/68.0'}
@@ -47,7 +53,7 @@ def get_movie(name, year):
     try:
         db_id = get_db_id(name, year)
     except Exception as e:
-        print(e)
+#        assert(e)
         db_id= None
     if not db_id:
         logging.error('获取豆瓣Subject ID出错\n')
